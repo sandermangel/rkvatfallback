5bf1c7f79e9c23cb2bdb12e8187a114fce75b613
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2014-11-21 12:08:04 +0100

    Added configuration fields for fallback VAT validation, added regex fallback and added modman file

--- app/code/community/Redkiwi/Rkvatfallback/Helper/Data.php
+++ app/code/community/Redkiwi/Rkvatfallback/Helper/Data.php
@@ -22,9 +22,11 @@ class Redkiwi_Rkvatfallback_Helper_Data extends Mage_Customer_Helper_Data
 		$requestParams['vatNumber'] = str_replace(array(' ', '-'), array('', ''), $vatNumber);
 		$requestParams['requesterCountryCode'] = $requesterCountryCode;
 		$requestParams['requesterVatNumber'] = str_replace(array(' ', '-'), array('', ''), $requesterVatNumber);
-		
+
+        $result = $gatewayResponse->getIsValid();
+
 		// try the EU VIES website
-		if (!$gatewayResponse->getIsValid())
+		if (!$result && Mage::getStoreConfig('customer/vat_services/vies_validation'))
 		{
 			$vat_url = 'http://ec.europa.eu/taxation_customs/vies/viesquer.do?';
 			$vat_url .= http_build_query(array(
@@ -55,10 +57,12 @@ class Redkiwi_Rkvatfallback_Helper_Data extends Mage_Customer_Helper_Data
 				$gatewayResponse->setRequestIdentifier('');
 				$gatewayResponse->setRequestSuccess(true);
 			}
+
+            $result = $gatewayResponse->getIsValid();
 		}
 		
 		// try the Isvat Appspot API
-		if (!$gatewayResponse->getIsValid())
+        if (!$result && Mage::getStoreConfig('customer/vat_services/isvat_validation'))
 		{
 			$vat_url = "http://isvat.appspot.com/{$requestParams['countryCode']}/{$requestParams['vatNumber']}/";
 			
@@ -83,8 +87,121 @@ class Redkiwi_Rkvatfallback_Helper_Data extends Mage_Customer_Helper_Data
 				$gatewayResponse->setRequestIdentifier('');
 				$gatewayResponse->setRequestSuccess(true);
 			}
+
+            $result = $gatewayResponse->getIsValid();
 		}
+
+        // Try regex
+        if (!$result && Mage::getStoreConfig('customer/vat_services/regex_validation'))
+        {
+            $gatewayResponse->setIsValid($this->vatRegexCheck($requestParams['vatNumber'], $requestParams['countryCode']));
+            $gatewayResponse->setRequestDate(date('Y/m/d H:i:s'));
+            $gatewayResponse->setRequestIdentifier('');
+            $gatewayResponse->setRequestSuccess(true);
+        }
 		
 		return $gatewayResponse;
 	}
+
+    /**
+     * Based on rules in http://ec.europa.eu/taxation_customs/vies/faqvies.do
+     * check if nif is valid
+     *
+     * Source; https://ellislab.com/forums/viewthread/159799
+     *
+     * @param string $vat
+     * @param string $country_iso
+     * @return boolean
+     */
+
+    public function vatRegexCheck($vat, $country_iso)
+    {
+        $country_iso = strtoupper($country_iso);
+        $regex = '';
+
+        switch ($country_iso)
+        {
+            case 'AT':
+                $regex = '/^U[0-9]{8}$/';
+                break;
+            case 'BE':
+                $regex = '/^0?[0-9]{*}$/';
+                break;
+            case 'CZ':
+                $regex = '/^[0-9]{8,10}$/';
+                break;
+            case 'DE':
+                $regex = '/^[0-9]{9}$/';
+                break;
+            case 'CY':
+                $regex = '/^[0-9]{8}[A-Z]$/';
+                break;
+            case 'DK':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'EE':
+                $regex = '/^[0-9]{9}$/';
+                break;
+            case 'GR':
+                $regex = '/^[0-9]{9}$/';
+                break;
+            case 'ES':
+                $regex = '/^[0-9A-Z][0-9]{7}[0-9A-Z]$/';
+                break;
+            case 'FI':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'FR':
+                $regex = '/^[0-9A-Z]{2}[0-9]{9}$/';
+                break;
+            case 'GB':
+                $regex = '/^([0-9]{9}|[0-9]{12})~(GD|HA)[0-9]{3}$/';
+                break;
+            case 'HU':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'IE':
+                $regex = '/^[0-9][A-Z0-9\\+\\*][0-9]{5}[A-Z]$/';
+                break;
+            case 'IT':
+                $regex = '/^[0-9]{11}$/';
+                break;
+            case 'LT':
+                $regex = '/^([0-9]{9}|[0-9]{12})$/';
+                break;
+            case 'LU':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'LV':
+                $regex = '/^[0-9]{11}$/';
+                break;
+            case 'MT':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'NL':
+                $regex = '/^[0-9]{9}B[0-9]{2}$/';
+                break;
+            case 'PL':
+                $regex = '/^[0-9]{10}$/';
+                break;
+            case 'PT':
+                $regex = '/^[0-9]{9}$/';
+                break;
+            case 'SE':
+                $regex = '/^[0-9]{12}$/';
+                break;
+            case 'SI':
+                $regex = '/^[0-9]{8}$/';
+                break;
+            case 'SK':
+                $regex = '/^[0-9]{10}$/';
+                break;
+            default:
+                return FALSE;
+                break;
+        }
+
+        $vat = str_replace($country_iso, '', $vat);
+        return (preg_match($regex,$vat));
+    }
 }
